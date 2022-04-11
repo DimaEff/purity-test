95202984243627c9fe91186afac5f29e9a9d8a68
DimaEff <69797190+DimaEff@users.noreply.github.com>
2022-04-11 21:35:36 +0300

    just a joke for my girlfriend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import "./joke.css";
 
 function App() {
     return (
         <div>
-            123
+            <h1 className={"gradient-text"}>Асечка - самая лучшая девочка на свете!!!!!!!</h1>
+            <h1 className={"gradient-text"}>Я очень очень в нее влюблен!!!!</h1>
+            <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <video style={{ height: 500, width: 250}} controls>
+                    <source src={require("./v.mp4")} type="video/mp4"/>
+                </video>
+            </div>
+            <h1 className={"gradient-text"}>Анекдот! Иногда я использую слова, значений которых не знаю, чтобы люди видели, какой я эрегированный.</h1>
         </div>
     );
 }
