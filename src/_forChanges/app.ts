a44f7a044b9faf543106fff795dcc66b3cf160ff
DimaEff <69797190+DimaEff@users.noreply.github.com>
2022-05-01 15:45:57 +0300

    added image for pinterest

--- src/_forChanges/app.ts
+++ src/_forChanges/app.ts
@@ -1,4 +1,4 @@
 export const QUESTIONS_COUNT = 100;
 
 // for changes
-export const IMAGE_FOR_PINTEREST_URL = "https://purity-test.com/assets/images/purity-test.png";
+export const IMAGE_FOR_PINTEREST_URL = "https://firebasestorage.googleapis.com/v0/b/purity-test-f6067.appspot.com/o/101-krasnaya-roza-v-korobke_1.jpg?alt=media&token=62937632-5563-4bce-9b6d-369f076952ad";
